84c12666a765be906b14abc65ae9d8e777bf29d2
kerlekaramol <amolkerlekar@gmail.com>
2022-11-18 16:41:27 +0530

    zindex

--- one.js
+++ one.js
@@ -1,2 +1,28 @@
-console.log('mumbai')
-console.log("pune")
+//string
+
+let firstName="Amol";
+let fatherName="Dattatray";
+let lastname='Kerlekar';
+console.log(firstName)
+console.log(fatherName)
+console.log(lastname)
+
+//number
+
+let mobileNo=7709427006;
+let experience=12.5;
+console.log(mobileNo)
+console.log(experience)
+
+//boolen
+let isvaccineted=true;
+console.log('vaccineted:'+isvaccineted)
+
+//null
+let total=null;
+console.log(total)
+
+//undefined
+let tax=undefined;
+console.log(tax)
+
